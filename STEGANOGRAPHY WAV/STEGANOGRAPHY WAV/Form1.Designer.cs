786988dc25b881c10d5ff5786e072cf2d72a3efb
Rahmat Siahaan <inspekturmuda@gmail.com>
2013-01-18 18:15:01 +0700

    Rubah Sedikit

--- STEGANOGRAPHY WAV/STEGANOGRAPHY WAV/Form1.Designer.cs
+++ STEGANOGRAPHY WAV/STEGANOGRAPHY WAV/Form1.Designer.cs
@@ -193,7 +193,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.BackColor = System.Drawing.Color.YellowGreen;
             this.ClientSize = new System.Drawing.Size(663, 594);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button4);
